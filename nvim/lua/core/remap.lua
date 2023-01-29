0ce4d3212904f8f0eb6d4a8813ad45c34708c017
d-a-weston <daniel.aaron.weston@gmail.com>
2023-01-29 16:13:21 +1000

    chore: cleaning up some vim settings

--- nvim/lua/core/remap.lua
+++ nvim/lua/core/remap.lua
@@ -33,9 +33,6 @@ vim.keymap.set("n", "<leader>cc", ":cclose<CR>")
 vim.keymap.set("n", "<C-j>", ":cnext<CR>zz")
 vim.keymap.set("n", "<C-k>", ":cprev<CR>zz")
 
--- Quick buffer substitution
+-- Easy buffer substitution
 vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
 vim.keymap.set("v", "<leader>s", "y:%s/<C-r>\"/<C-r>\"/gI<Left><Left><Left>")
-
--- Make current file executable
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
