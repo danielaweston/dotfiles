b9312a15c3927678f71dd5f12093934e19c8b2f5
d-a-weston <daniel.aaron.weston@gmail.com>
2023-05-15 09:34:42 +1000

    feat: add cloudformation custom tags to lsp

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -33,7 +33,28 @@ lsp.configure("terraformls", {
 lsp.configure("yamlls", {
   settings = {
     yaml = {
-      keyOrdering = false
+      keyOrdering = false,
+      customTags = {
+        "!Base64 scalar",
+        "!Cidr scalar",
+        "!And sequence",
+        "!Equals sequence",
+        "!If sequence",
+        "!Not sequence",
+        "!Or sequence",
+        "!Condition scalar",
+        "!FindInMap sequence",
+        "!GetAtt scalar",
+        "!GetAtt sequence",
+        "!GetAZs scalar",
+        "!ImportValue scalar",
+        "!Join sequence",
+        "!Select sequence",
+        "!Split sequence",
+        "!Sub scalar",
+        "!Transform mapping",
+        "!Ref scalar",
+      },
     }
   }
 })
