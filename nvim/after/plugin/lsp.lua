8419a1933eb62233b68a048434a022c37a80e690
d-a-weston <daniel.aaron.weston@gmail.com>
2023-07-10 14:08:48 +1000

    chore: fix up null ls and zero lsp auto formatting errors

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -102,6 +102,8 @@ lsp.on_attach(function(client, bufnr)
   vim.opt.formatoptions:remove("o")
   vim.opt.formatoptions:remove("r")
 
+  lsp.buffer_autoformat()
+
   -- Issues with semantic tokens provider
   -- https://github.com/neovim/nvim-lspconfig/issues/2552
   if client.server_capabilities.semanticTokensProvider then
@@ -109,29 +111,26 @@ lsp.on_attach(function(client, bufnr)
   end
 end)
 
+lsp.format_on_save({
+  format_opts = {
+    async = false,
+    timeout_ms = 10000,
+  },
+  servers = {
+    ['null-ls'] = { 'javascript', 'typescript' }
+  }
+})
+
 lsp.setup()
 
 vim.diagnostic.config({
   virtual_text = true,
 })
 
-local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
-local event = "BufWritePre"
-local async = event == "BufWritePost"
+local null_opts = lsp.build_options('null-ls', {})
 
 null_ls.setup({
   on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      -- format on save
-      vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
-      vim.api.nvim_create_autocmd(event, {
-        buffer = bufnr,
-        group = group,
-        callback = function()
-          vim.lsp.buf.format({ bufnr = bufnr, async = async })
-        end,
-        desc = "[lsp] format on save",
-      })
-    end
+    null_opts.on_attach(client, bufnr)
   end,
 })
