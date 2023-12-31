60ceb3d5b9843683382091e1d231dd1f2242bca9
d-a-weston <daniel.aaron.weston@gmail.com>
2023-12-31 10:22:02 +1000

    feat: lazy with remap, options and color-scheme

---
+++ nvim/lua/options.lua
@@ -0,0 +1,43 @@
+-- Indentation options
+local tabsize = 2
+vim.opt.tabstop = tabsize
+vim.opt.softtabstop = tabsize
+vim.opt.shiftwidth = tabsize
+vim.opt.expandtab = true
+vim.opt.smarttab = true
+
+-- Line number options
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.numberwidth = 2
+
+-- Case semi-sensitive search
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Visual options
+vim.opt.wrap = false
+vim.opt.scrolloff = 10
+vim.opt.signcolumn = "yes"
+vim.opt.colorcolumn = "80"
+vim.opt.cursorline = true
+vim.opt.termguicolors = true
+
+-- Undo history
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+-- Misc options
+vim.opt.autoread = true                -- Re-read files when changed outside of vim
+vim.opt.clipboard = "unnamedplus"      -- Use system clipboard
+vim.opt.wildmode = "longest:full,full" -- Command-line completion mode
+
+vim.opt.updatetime = 50
+
+-- Cannot remember why we do this
+-- vim.opt.backspace = "indent,eol,start"
+-- vim.opt.splitbelow = true
+-- vim.opt.splitright = true
---
+++ nvim/lua/remaps.lua
@@ -0,0 +1,32 @@
+-- Rebind leader to space
+vim.g.mapleader = " "
+
+-- Easy escape in Insert mode
+vim.keymap.set("i", "jk", "<ESC>")
+
+-- Clear search highlighting
+vim.keymap.set("n", "<leader>/", ":noh<CR>")
+
+-- Bind redo to r
+vim.keymap.set("n", "r", "<C-r>")
+
+-- Center after movements
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Move visual blocks
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Reselect visual selection after indenting
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
+
+-- Easy buffer substitution
+vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
+vim.keymap.set("v", "<leader>s", "y:%s/<C-r>\"/<C-r>\"/gI<Left><Left><Left>")
+
+-- Open project finder
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww ~/.local/bin/tmuxer<CR>")
