3a394ca08fd7c5daea9e76c8fa20957e645555ab
d-a-weston <daniel.aaron.weston@gmail.com>
2024-01-01 18:00:36 +1000

    feat: added lsp and mason

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,14 +1,27 @@
 return {
   {
+    "nvim-telescope/telescope-ui-select.nvim",
+  },
+  {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.5",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       local builtin = require("telescope.builtin")
 
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
+      })
+
       vim.keymap.set("n", "<C-p>", builtin.git_files, {})
       vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
       vim.keymap.set("n", "<leader>pg", builtin.live_grep, {})
+
+      require("telescope").load_extension("ui-select")
     end,
   },
 }
