6fec41a1a9c8437efe62787251c6fbe49f06c00f
d-a-weston <daniel.aaron.weston@gmail.com>
2024-01-03 13:51:08 +1000

    feat: added toggle to git status

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -1,7 +1,20 @@
 return {
-	"tpope/vim-fugitive",
-	config = function()
-		vim.keymap.set("n", "<leader>gs", ":Git<CR>")
-		vim.keymap.set("n", "<leader>gb", ":Git blame<CR>")
-	end,
+  "tpope/vim-fugitive",
+  config = function()
+    vim.keymap.set("n", "<leader>gb", ":Git blame<CR>")
+
+    -- Toggle git status window
+    -- https://github.com/tpope/vim-fugitive/issues/1274
+    vim.keymap.set("n", "<leader>gs", function()
+      local winids = vim.api.nvim_list_wins()
+      for _, id in pairs(winids) do
+        local status = pcall(vim.api.nvim_win_get_var, id, "fugitive_status")
+        if status then
+          vim.api.nvim_win_close(id, false)
+          return
+        end
+      end
+      vim.cmd("Git")
+    end)
+  end,
 }
