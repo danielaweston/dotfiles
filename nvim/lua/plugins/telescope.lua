e4cb86f7b0146ccc66197c52a3333422b8bbf67d
d-a-weston <daniel.aaron.weston@gmail.com>
2024-01-09 10:34:17 +1000

    chore: refactored telescope and added diagnostic keymap

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,57 +1,58 @@
 return {
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-	},
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.5",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			local builtin = require("telescope.builtin")
-			local actions = require("telescope.actions")
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.5",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope-ui-select.nvim",
+    },
+    config = function()
+      local builtin = require("telescope.builtin")
+      local actions = require("telescope.actions")
 
-			require("telescope").setup({
-				defaults = {
-					path_display = { "truncate" },
-					vimgrep_arguments = {
-						"rg",
-						"--color=never",
-						"--no-heading",
-						"--with-filename",
-						"--line-number",
-						"--column",
-						"--smart-case",
-						"--hidden",
-						"--trim",
-						"--glob",
-						"!**/.git/*",
-					},
-					mappings = { i = { ["<esc>"] = actions.close } },
-				},
-				pickers = {
-					find_files = {
-						hidden = true,
-						find_command = {
-							"rg",
-							"--files",
-							"--hidden",
-							"--glob",
-							"!**/.git/*",
-						},
-					},
-				},
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
-					},
-				},
-			})
+      require("telescope").setup({
+        defaults = {
+          path_display = { "truncate" },
+          vimgrep_arguments = {
+            "rg",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+            "--hidden",
+            "--trim",
+            "--glob",
+            "!**/.git/*",
+          },
+          mappings = { i = { ["<esc>"] = actions.close } },
+        },
+        pickers = {
+          find_files = {
+            hidden = true,
+            find_command = {
+              "rg",
+              "--files",
+              "--hidden",
+              "--glob",
+              "!**/.git/*",
+            },
+          },
+        },
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
+      })
 
-			vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-			vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>pg", builtin.live_grep, {})
+      vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+      vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>pg", builtin.live_grep, {})
+      vim.keymap.set("n", "<leader>pd", builtin.diagnostics, {})
 
-			require("telescope").load_extension("ui-select")
-		end,
-	},
+      require("telescope").load_extension("ui-select")
+    end,
+  },
 }
