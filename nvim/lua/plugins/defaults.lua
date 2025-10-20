c03c1f0c14f2293c196b9e492a0f158a5da3090e
danielaweston <daniel.aaron.weston@gmail.com>
2025-10-20 11:35:01 +1000

    chore: a bit more lazyvim config

---
+++ nvim/lua/plugins/defaults.lua
@@ -0,0 +1,22 @@
+-- since this is just an example spec, don't actually load anything here and return an empty spec
+-- stylua: ignore
+if true then return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        hidden = true, -- Show hidden files in file pickers
+        ignored = true, -- Show files ignored by `.gitignore`
+      },
+    },
+  },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      cmdline = {
+        view = "cmdline"
+      }
+    }
+  },
+} end
